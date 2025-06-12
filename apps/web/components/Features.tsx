cea988e3404fc1331f5aca6c50dcc5e60177b654
Tushar Chawre <tusharchawre0240@gmail.com>
2025-06-12 21:52:41 +0530

    Testimonial Stuff

--- apps/web/components/Features.tsx
+++ apps/web/components/Features.tsx
@@ -8,21 +8,28 @@ import { SettleUp } from "./ui/gridElem/settleUp";
 
 export const Features = () => {
   return (
-    <div className="grid-col-1 grid h-[1080px] w-full grid-rows-2 gap-4 px-8 py-2 md:grid-cols-3 md:px-16">
-      <div className="flex flex-col items-start justify-around gap-4 overflow-hidden rounded-xl bg-neutral-100 px-10 py-6 md:col-span-2">
-        <BillSplit />
-      </div>
+    <div className="h-[1080px] my-12 mb-24 w-full px-8 py-2 md:px-32">
+      <h1 className="w-full text-center font-semibold text-3xl">Everything you need</h1>
+      <p className="text-center mx-auto my-4 w-3xl text-muted-background text-sm">
+        Split bills effortlessly, track expenses in real-time, and settle up with friends - all in one place. Our smart features make managing shared expenses as easy as splitting a pizza.
+      </p>
+      <div className="grid-col-1 grid my-4 grid-rows-2 gap-4  md:grid-cols-3 ">
 
-      <div className="flex flex-col items-start justify-around rounded-xl bg-neutral-100 px-8 py-6 md:col-span-1">
-        <ScanSplit />
-      </div>
+        <div className="flex flex-col items-start justify-around gap-4 overflow-hidden rounded-xl bg-neutral-100 px-10 py-6 md:col-span-2">
+          <BillSplit />
+        </div>
 
-      <div className="flex flex-col items-start justify-around rounded-xl bg-neutral-100 px-10 py-6 md:col-span-1">
-        <TrackExpenses />
-      </div>
+        <div className="flex flex-col items-start justify-around rounded-xl bg-neutral-100 px-8 py-6 md:col-span-1">
+          <ScanSplit />
+        </div>
 
-      <div className="flex flex-col items-start justify-around rounded-xl bg-neutral-100 px-10 py-6 md:col-span-2">
-        <SettleUp />
+        <div className="flex flex-col items-start justify-around rounded-xl bg-neutral-100 px-10 py-6 md:col-span-1">
+          <TrackExpenses />
+        </div>
+
+        <div className="flex flex-col items-start justify-around rounded-xl bg-neutral-100 px-10 py-6 md:col-span-2">
+          <SettleUp />
+        </div>
       </div>
     </div>
   );
--- apps/web/components/Testimonials.tsx
+++ apps/web/components/Testimonials.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "motion/react"
 
 export const Testimonials = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start 50%", "end 80%"],
+
+  });
+
+  const left = useTransform(scrollYProgress, [0, 0.8], ["-0.5rem", "-20rem"]);
+  const right = useTransform(scrollYProgress, [0, 0.8], ["-0.5rem", "-20rem"]);
+
   return (
-    <div className="h-screen w-full overflow-hidden">
-      <div className="relative flex h-full w-full flex-col items-center justify-center gap-2">
-        <div className="absolute inset-0 blur-[100px]">
-          <div className="absolute -top-[30%] -left-80 z-10 aspect-square w-4xl rounded-full bg-white" />
+    <motion.div ref={ref} className="h-screen w-full my-12 overflow-x-clip">
+      <motion.div className="relative flex h-full w-full flex-col items-center justify-center gap-2">
+        <motion.div className="absolute inset-0 blur-[100px]">
+          <motion.div
+            style={{ left }}
+            className="absolute -top-[30%] -left-80 z-10 aspect-square w-4xl rounded-full bg-white"
+          />
           <div className="absolute -top-[10%] -left-80 aspect-square w-4xl rounded-full bg-[#8C00FF]" />
-          <div className="absolute -right-80 -bottom-[30%] z-10 aspect-square w-4xl rounded-full bg-white" />
+          <motion.div style={{ right }} className="absolute -right-80 -bottom-[30%] z-10 aspect-square w-4xl rounded-full bg-white" />
           <div className="absolute -right-80 -bottom-[10%] aspect-square w-4xl rounded-full bg-[#00FFF2]" />
-        </div>
+        </motion.div>
 
         <p className="relative z-50 text-sm font-semibold text-[#0077B7]">
           What people are saying
@@ -18,7 +33,7 @@ export const Testimonials = () => {
           Trusted By Me
         </p>
         <p className="relative z-50 text-sm text-neutral-400">(Singularly)</p>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
